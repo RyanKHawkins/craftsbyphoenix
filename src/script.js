3f483611e574d940464ebf56a50ae485ae179ab4
Ryan Hawkins <rkhawkins80@hotmail.com>
2024-08-18 21:08:29 -0500

    added 'all' option to drop-down and placed more placeholder text

--- src/script.js
+++ src/script.js
@@ -2,10 +2,11 @@ const categorySelector = document.querySelector("#category-selector");
 const categoryHeader = document.querySelector("#category-header");
 
 categorySelector.addEventListener("change", () => {
-    categoryHeader.innerText = categoryDict[categorySelector.value]
+    categoryHeader.innerText = categoryDisplayDict[categorySelector.value]
     console.log(categorySelector.value)
 })
-const categoryDict = {
+const categoryDisplayDict = {
+    "all": "All",
     "bracelets": "Bracelets",
     "tails": "Tails",
     "miscellaneous": "Miscellaenous"
