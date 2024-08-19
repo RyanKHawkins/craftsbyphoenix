74f94ca690b3c4de17b7fe4cfe00e1d647483e64
Ryan Hawkins <rkhawkins80@hotmail.com>
2024-08-18 20:45:14 -0500

    added selection display

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 const categorySelector = document.querySelector("#category-selector");
+const categoryHeader = document.querySelector("#category-header");
 
 categorySelector.addEventListener("change", () => {
-    console.log("changed")
+    categoryHeader.innerText = categoryDict[categorySelector.value]
     console.log(categorySelector.value)
 })
+const categoryDict = {
+    "bracelets": "Bracelets",
+    "tails": "Tails",
+    "miscellaneous": "Miscellaenous"
+}
