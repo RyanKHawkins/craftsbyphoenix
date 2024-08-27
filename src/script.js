c9f4e0eb199fb85d67a798ec7ecdcb9bbd0ce7d6
Ryan Hawkins <rkhawkins80@hotmail.com>
2024-08-26 21:11:15 -0500

    added button access from the DOM

--- src/script.js
+++ src/script.js
@@ -3,6 +3,9 @@ import "./products.js"
 const categorySelector = document.querySelector("#category-selector");
 const categoryHeader = document.querySelector("#category-header");
 
+const prevButton = document.querySelector("#prev-button");
+const nextButton = document.querySelector("#next-button");
+
 categorySelector.addEventListener("change", () => {
     categoryHeader.innerText = categoryDisplayDict[categorySelector.value]
     console.log(categorySelector.value)
